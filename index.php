226cacab4112597cb6ea8605fcd2388bf766be57
ZorayaF <azflorez@uniboyaca.edu.co>
2023-10-06 20:42:31 -0500

    Creación del menú de opciones

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Taller con HTML y PHP</title>
+</head>
+<body>
+    <h1>Menú de navegación</h1>
+    <a href="">Lista de números</a><br>
+    <a href="">Cálculo del área de figuras geométricas</a><br>
+    <a href="">Cálculo del promedio de asignaturas</a><br>
+    <a href="">Gestón de docentes y cursos</a><br>
+
+    
+</body>
+</html>
