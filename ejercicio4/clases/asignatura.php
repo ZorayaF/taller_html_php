2b07af4113c0216adc113a0cff252f1012411a70
ZorayaF <azflorez@uniboyaca.edu.co>
2023-10-14 09:29:29 -0500

    Creacion clases docente y estudiante

---
+++ ejercicio4/clases/asignatura.php
@@ -0,0 +1,21 @@
+<?php
+class Asignatura{
+    private $codigo;
+    private $nombre;
+    private $docente;
+
+    function __construct($codigo, $nombre, $docente){
+        $this->$codigo = $codigo;
+        $this->$nombre = $nombre;
+        $this->$docente = $docente;
+    }
+
+    function get($prop){
+        return $this->$prop;
+    }
+
+    function set($prop, $value){
+        $this->$prop = $value;
+    }
+}
+?>
---
+++ ejercicio4/interfaz/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestión</title>
+</head>
+<body>
+    <h1>Administración</h1>
+    <form action="">
+        <input type="submit" value="Profesores">
+    </form>
+    <form action="">
+        <input type="submit" value="Asignaturas">
+    </form>
+</body>
+</html>
