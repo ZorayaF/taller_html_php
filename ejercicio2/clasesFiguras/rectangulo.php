c248530dd6af637f88d5b83e64c3b0cacc034a89
ZorayaF <azflorez@uniboyaca.edu.co>
2023-10-14 08:33:54 -0500

    Prueba

--- ejercicio2/clasesFiguras/rectangulo.php
+++ ejercicio2/clasesFiguras/rectangulo.php
@@ -3,13 +3,12 @@ class Rectangulo extends Figura{
     private $altura;
     private $base;
 
-    public function __construct($base, $altura){
-        $this->base=$base;
-        $this->altura=$altura;
+    public function __construct($baseR, $alturaR){
+        $this->base=$baseR;
+        $this->altura=$alturaR;
     }
-    
     public function calcularArea(){
-        return $this->base*$this->altura;;
+        return $this->base;
     }
 }
 ?>
--- ejercicio2/interfaz/procesamientoDatos.php
+++ ejercicio2/interfaz/procesamientoDatos.php
@@ -12,22 +12,26 @@ if(isset($_POST['areaCirculo'])){
     $circulo = new Circulo($radio);
     $area = $circulo->calcularArea();
     echo 'El área del circulo es: '.$area;
+
 }else if(isset($_POST['areaCuadrado'])){
     $lado = floatval($_POST['lado']);
     $cuadrado = new Cuadrado($lado);
     $area = $cuadrado->calcularArea();
     echo 'El área del cuadrado es: '.$area;
+
 }else if(isset($_POST['areaRectangulo'])){
-    $base = floatval($_POST['baseR']);
-    $altura = $_POST['alturaR'];
-    $rectangulo = new Rectangulo($base, $altura);
+    $baseR = floatval($_POST['baseR']);
+    $alturaR = floatval($_POST['alturaR']);
+    $rectangulo = new Rectangulo($baseR, $alturaR);
     $area = $rectangulo->calcularArea();
     echo 'El área del rectángulo es: '.$area;
-}else{
-    $base = $_POST['baseT'];
-    $altura = floatval($_POST['alturaT']);
-    $triangulo = new Triangulo($base, $altura);
+
+}else if(isset($_POST['areaTriangulo'])){
+    $baseT = floatval($_POST['baseT']);
+    $alturaT = floatval($_POST['alturaT']);
+    $triangulo = new Triangulo($baseT, $alturaT);
     $area = $triangulo->calcularArea();
     echo 'El área del triángulo es: '.$area;
+
 }
 ?>
