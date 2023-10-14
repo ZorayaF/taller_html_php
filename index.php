7004adb6d8b69cba126ae2a5789e213473d92de0
ZorayaF <azflorez@uniboyaca.edu.co>
2023-10-13 20:07:30 -0500

    Correccion de los links del menu

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ use App\ejercicio1\index;
 </head>
 <body>
     <h1>Menú de navegación</h1>
-    <a href="/ejercicio1/index.php">Lista de números</a><br>
-    <a href="/ejercicio2/interfaz/iuFiguras.php">Cálculo del área de figuras geométricas</a><br>
+    <a href="/taller_html_php/ejercicio1/index.php">Lista de números</a><br>
+    <a href="/taller_html_php/ejercicio2/interfaz/iuFiguras.php">Cálculo del área de figuras geométricas</a><br>
     <a href="">Cálculo del promedio de asignaturas</a><br>
     <a href="">Gestón de docentes y cursos</a><br>
     
