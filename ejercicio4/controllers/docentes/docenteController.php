7ac3adb036d3d7554c4b24b0874f01798db17beb
ZorayaF <azflorez@uniboyaca.edu.co>
2023-10-20 12:11:07 -0500

    Se puede observar la tabla docentes y el nombre de su ocupacion

--- ejercicio4/controllers/docentes/docenteController.php
+++ ejercicio4/controllers/docentes/docenteController.php
@@ -6,18 +6,30 @@ use App\controllers\EntityController;
 use App\models\Docente;
 
 class DocenteController extends EntityController{
-  private $dataTable = 'docentes';
+  private $docenteDT = 'docentes';
+  private $ocupacionDT = 'ocupaciones';
+
+
+  private $codD= 'cod';
+  private $nombreD = 'nombre';
+  private $ocupacionD = 'idOcupacion';
+  private $codigoO = 'id';
+  private $nombreO = 'nombre';
 
   function allData(){
-    $sql = "select * from " . $this->dataTable;
+    $sql = "SELECT D." . $this->codD . " AS CodigoDocente , D." . $this->nombreD . " AS NombreDocente, O." . $this->nombreO . " AS NombreOcupacion
+            FROM " . $this->docenteDT . " D
+            JOIN " . $this->ocupacionDT . " O ON D." . $this->ocupacionD . " = O." . $this->codigoO;;
+
+
     $resultSQL = $this->execSql($sql);
     $lista = [];
     if($resultSQL->num_rows > 0){
         while($item = $resultSQL->fetch_assoc()){
             $docente = new Docente();
-            $docente->set('cod', $item['cod']);
-            $docente->set('nombre',$item['nombre']);
-            $docente->set('idOcupacion',$item['idOcupacion']);
+            $docente->set('cod', $item['CodigoDocente']);
+            $docente->set('nombre',$item['NombreDocente']);
+            $docente->set('idOcupacion',$item['NombreOcupacion']);
             array_push($lista, $docente);
         }
     } 
